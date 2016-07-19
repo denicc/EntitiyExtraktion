eda02e70c4b77cb76c75735d1edf784055e22a10
denicc <denic@outlook.com>
2016-07-19 16:44:35 +0200

    Updating

--- EntitiyExtraktion/src/main/java/mongodb/connection/src/MongoDBConnect.java
+++ EntitiyExtraktion/src/main/java/mongodb/connection/src/MongoDBConnect.java
@@ -1,5 +1,6 @@
 package mongodb.connection.src;
 
+import com.mongodb.BasicDBObject;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
@@ -18,7 +19,7 @@ public class MongoDBConnect {
 
 		MongoDBConnect mongodbconn = new MongoDBConnect();
 		try {
-			mongodbconn.getRawData(mongodbconn.getConnection("localhost", 27017));
+			mongodbconn.getRawData(mongodbconn.getConnection("138.201.125.161", 27017, "crawlDB"));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -26,14 +27,30 @@ public class MongoDBConnect {
 		
 	}
 
-	private void getRawData(MongoDatabase mongoDatabase) {
+	private void getRawData(MongoDatabase db) {
+		
+		System.out.println(db.getName());
+		
+		BasicDBObject searchQuery = new BasicDBObject();
+		searchQuery.put("baseUrl","http://www.bergbier.de/");
+		
+		FindIterable<Document> iterable = db.getCollection("webpage").find().limit(8000);
+
+		
+		
+		iterable.forEach(new Block<Document>() {
+		    public void apply(final Document document) {
+		        System.out.println(document);
+		    }
+		});
+		
 		
 		System.out.println("Done");
 	}
 
-	private MongoDatabase getConnection(String host, int port) {
+	private MongoDatabase getConnection(String host, int port, String db) {
 		MongoClient mongoClient = new MongoClient(host, port);
-		MongoDatabase mongodb = mongoClient.getDatabase("crawlDB");
+		MongoDatabase mongodb = mongoClient.getDatabase(db);
 		
 		return mongodb;
 		
