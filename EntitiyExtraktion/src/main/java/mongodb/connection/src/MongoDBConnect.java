d20e7cf81a37df81379753f25aca69d10343d95b
denicc <denic@outlook.com>
2016-07-19 16:13:06 +0200

    updating maven project

--- EntitiyExtraktion/src/main/java/mongodb/connection/src/MongoDBConnect.java
+++ EntitiyExtraktion/src/main/java/mongodb/connection/src/MongoDBConnect.java
@@ -3,13 +3,22 @@ package mongodb.connection.src;
 import com.mongodb.MongoClient;
 import com.mongodb.client.MongoDatabase;
 
+import org.bson.Document;
+
+import com.mongodb.Block;
+import com.mongodb.client.FindIterable;
+
+import static com.mongodb.client.model.Filters.*;
+import static com.mongodb.client.model.Sorts.ascending;
+import static java.util.Arrays.asList;
+
 public class MongoDBConnect {
 
 	public static void main(String[] args) {
 
 		MongoDBConnect mongodbconn = new MongoDBConnect();
 		try {
-			mongodbconn.getRawData(mongodbconn.getConnection());
+			mongodbconn.getRawData(mongodbconn.getConnection("localhost", 27017));
 			
 		} catch (Exception e) {
 			e.printStackTrace();
@@ -22,8 +31,8 @@ public class MongoDBConnect {
 		System.out.println("Done");
 	}
 
-	private MongoDatabase getConnection() {
-		MongoClient mongoClient = new MongoClient("localhost, 27017");
+	private MongoDatabase getConnection(String host, int port) {
+		MongoClient mongoClient = new MongoClient(host, port);
 		MongoDatabase mongodb = mongoClient.getDatabase("crawlDB");
 		
 		return mongodb;
