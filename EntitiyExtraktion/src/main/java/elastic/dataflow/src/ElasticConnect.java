429a7a00795e4344109ac9b73fbbc310b9bd48cc
denicc <denic@outlook.com>
2016-08-03 11:03:35 +0200

    Updatinig HostList from Elasticsearch and getDataPerHost Method

--- EntitiyExtraktion/src/main/java/elastic/dataflow/src/ElasticConnect.java
+++ EntitiyExtraktion/src/main/java/elastic/dataflow/src/ElasticConnect.java
@@ -1,7 +1,10 @@
 package elastic.dataflow.src;
 
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.search.SearchType;
@@ -9,55 +12,89 @@ import org.elasticsearch.client.Client;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.transport.InetSocketTransportAddress;
 import org.elasticsearch.index.engine.Engine.Get;
+import org.elasticsearch.index.mapper.object.ObjectMapper;
 import org.elasticsearch.index.query.FilterBuilders;
 import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHitField;
+import org.elasticsearch.search.SearchHits;
 
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
 
 public class ElasticConnect {
 
-	private static String ELASTIC_HOST = "127.0.0.1";
+	private static String ELASTIC_HOST = "138.201.125.161";
 	//Muss 9300 sein, da die JAVA-API diesen Port nutzt
 	private static int ELASTIC_PORT = 9300;
+	private String dateFormat = "yyyy-MM-dd'T'HH:mm:ssX";
 	
 	
-	
-	
-
-
 	public static void main(String[] args) {
 
 		
 		ElasticConnect elastic = new ElasticConnect();
 		
 		Client client = elastic.getClient(ELASTIC_HOST, ELASTIC_PORT);
-		elastic.startElasticExtraktion(client);
+		elastic.getAllHosts(client);
+		//elastic.getElasticData(client);
 		
 	}
 	
 
-	public void startElasticExtraktion(Client client){
-		
+	
+	
+	private void getAllHosts(Client client) {
+		// TODO Auto-generated method stub
 		SearchResponse response = client.prepareSearch("nutch")
-		        .setTypes("text/html")
-		        .execute()
-		        .actionGet();
-		
-		
-		
-		
-		
+			    .setTypes("doc")
+			    .setSource("host")             
+			    .setFrom(0)
+			    .setSize(1000)              
+			    .execute().actionGet();
+			Set<String> hostList = new HashSet<String>();
+
+			for (SearchHit hit : response.getHits()) { 
+				hostList.add(hit.getSource().get("host").toString());
+			    // do something with the id value
+			}
+
+			hostList.forEach(System.out::println);
+	
+			getDataPerHost(client, hostList);
+			
 	}
-	
-	
-	private void getElasticData(Client client) {
+
+
+
+
+	public void getDataPerHost(Client client, Set<String> hostList){
+		
+		System.out.println(hostList.size());
+		
+		
+		
+		SearchResponse response = client.prepareSearch("nutch").setSearchType(
+		          SearchType.DFS_QUERY_THEN_FETCH).setQuery(
+		          QueryBuilders.matchQuery("host", "www.ahlers-ag.com")).setFrom(0).setSize(60).setExplain(true)
+		          .execute().actionGet();
+		 
+		      SearchHit[] docs = response.getHits().getHits();
+		      
+		      for (int i = 0; i < docs.length; i++) {
+				System.out.println(docs[i].sourceAsString());
+		      }
+		      
+		      System.out.println("size is " + docs.length);
+		      client.close();
 
 		
 		
 	}
-
+	 
+		
+	
 
 	public Client getClient(String ELASTIC_HOST, int ELASTIC_PORT){
 		
