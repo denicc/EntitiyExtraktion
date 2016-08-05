6db74731105696cb58e8243a34ec8936c6d89ee6
denicc <denic@outlook.com>
2016-08-05 14:44:06 +0200

    added rules

--- EntitiyExtraktion/src/main/java/stanfordnlp/src/StanfordExample.java
+++ EntitiyExtraktion/src/main/java/stanfordnlp/src/StanfordExample.java
@@ -3,15 +3,23 @@ package stanfordnlp.src;
 import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.Properties;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import edu.stanford.nlp.ie.AbstractSequenceClassifier;
 import edu.stanford.nlp.ie.crf.CRFClassifier;
 import edu.stanford.nlp.io.IOUtils;
 import edu.stanford.nlp.ling.CoreAnnotations;
 import edu.stanford.nlp.ling.CoreAnnotations.AnswerAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.NamedEntityTagAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.SentencesAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TextAnnotation;
+import edu.stanford.nlp.ling.CoreAnnotations.TokensAnnotation;
 import edu.stanford.nlp.ling.CoreLabel;
 import edu.stanford.nlp.ling.tokensregex.CoreMapExpressionExtractor;
 import edu.stanford.nlp.ling.tokensregex.MatchedExpression;
@@ -27,7 +35,8 @@ import edu.stanford.nlp.util.StringUtils;
 
 public class StanfordExample {
 	
-	
+	final Logger LOG = LoggerFactory.getLogger(StanfordExample.class);
+
 	public static void main(String[] args) {
 			
 		
@@ -35,6 +44,7 @@ public class StanfordExample {
 	}
 	
 	public void getNE(String content) throws ClassCastException, ClassNotFoundException, IOException{
+
 
 		String serializedClassifier = "classifier/stanford/dewac_175m_600.crf.ser.gz";
 	    AbstractSequenceClassifier<CoreLabel> classifier = CRFClassifier.getClassifier(serializedClassifier);
@@ -75,33 +85,123 @@ public class StanfordExample {
 	        StanfordCoreNLP pipeline = new StanfordCoreNLP(germanProperties);
 	       
 	  
-	        pipeline.annotate(germanAnnotation);
-	       
-	        List<CoreMap> sentences = germanAnnotation.get(CoreAnnotations.SentencesAnnotation.class);
+	        
+	        String[] tests =
+	            {
+	                "Partial invoice (€100,000, so roughly 40%) for the consignment C27655 we shipped on 15th August to London from the Make Believe Town depot. INV2345 is for the balance.. Customer contact (Sigourney) says they will pay this on the usual credit terms (30 days)."
+	            };
+	        List<String> tokens = new ArrayList<String>();
 
-	        for (CoreMap sentence : sentences) {
-	     
-	         System.out.println(extractor.getValue("SCHLAU"));
-	         System.out.println(sentence);
-	         System.out.println(extractor.extractExpressions(sentence));
-	          List<MatchedExpression> matchedExpressions = extractor.extractExpressions(sentence);
-	          System.out.println(matchedExpressions.size());
-	          for (MatchedExpression matched:matchedExpressions) {
-	            // Print out matched text and value
-	            System.out.println("Matched expression: " + matched.getText() + " with value " + matched.getValue());
-	            // Print out token information
-	            CoreMap cm = matched.getAnnotation();
-	            for (CoreLabel token : cm.get(CoreAnnotations.TokensAnnotation.class)) {
-	              String word = token.get(CoreAnnotations.TextAnnotation.class);
-	              String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
-	              String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
-	              String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
-	              System.out.println("  Matched token: " + "word="+word + ", lemma="+lemma + ", pos=" + pos + ", ne=" + ne);
+	        for (String s : tests) {
+
+	          // run all Annotators on the passed-in text
+	          Annotation document = new Annotation(s);
+	          pipeline.annotate(document);
+
+	          // these are all the sentences in this document
+	          // a CoreMap is essentially a Map that uses class objects as keys and has values with
+	          // custom types
+	          List<CoreMap> sentences = document.get(SentencesAnnotation.class);
+	          StringBuilder sb = new StringBuilder();
+	          
+	          //I don't know why I can't get this code out of the box from StanfordNLP, multi-token entities
+	          //are far more interesting and useful..
+	          //TODO make this code simpler..
+	          for (CoreMap sentence : sentences) {
+	            // traversing the words in the current sentence, "O" is a sensible default to initialise
+	            // tokens to since we're not interested in unclassified / unknown things..
+	            String prevNeToken = "O";
+	            String currNeToken = "O";
+	            boolean newToken = true;
+	            for (CoreLabel token : sentence.get(TokensAnnotation.class)) {
+	              currNeToken = token.get(NamedEntityTagAnnotation.class);
+	              String word = token.get(TextAnnotation.class);
+	              // Strip out "O"s completely, makes code below easier to understand
+	              if (currNeToken.equals("O")) {
+	                // LOG.debug("Skipping '{}' classified as {}", word, currNeToken);
+	                if (!prevNeToken.equals("O") && (sb.length() > 0)) {
+	                  handleEntity(prevNeToken, sb, tokens);
+	                  newToken = true;
+	                }
+	                continue;
+	              }
+
+	              if (newToken) {
+	                prevNeToken = currNeToken;
+	                newToken = false;
+	                sb.append(word);
+	                continue;
+	              }
+
+	              if (currNeToken.equals(prevNeToken)) {
+	                sb.append(" " + word);
+	              } else {
+	                // We're done with the current entity - print it out and reset
+	                // TODO save this token into an appropriate ADT to return for useful processing..
+	                handleEntity(prevNeToken, sb, tokens);
+	                newToken = true;
+	              }
+	              prevNeToken = currNeToken;
 	            }
 	          }
+	          
+	          //TODO - do some cool stuff with these tokens!
+	          LOG.debug("We extracted {} tokens of interest from the input text", tokens.size());
 	        }
+	      }
+	      private void handleEntity(String inKey, StringBuilder inSb, List inTokens) {
+	        LOG.debug("'{}' is a {}", inSb, inKey);
+	        inTokens.add(new EmbeddedToken(inKey, inSb.toString()));
+	        inSb.setLength(0);
+	      }
+
+
+	    }
+	    class EmbeddedToken {
+
+	      private String name;
+	      private String value;
+
+	      public String getName() {
+	        return name;
+	      }
+
+	      public String getValue() {
+	        return value;
+	      }
+
+	      public EmbeddedToken(String name, String value) {
+	        super();
+	        this.name = name;
+	        this.value = value;
+	      }
+	        
+	       
+//	        List<CoreMap> sentences = germanAnnotation.get(CoreAnnotations.SentencesAnnotation.class);
+//
+//	        for (CoreMap sentence : sentences) {
+//	     
+//	         System.out.println(extractor.getValue("SCHLAU"));
+//	         System.out.println(sentence);
+//	         System.out.println(extractor.extractExpressions(sentence));
+//	          List<MatchedExpression> matchedExpressions = extractor.extractExpressions(sentence);
+//	          System.out.println(matchedExpressions.size());
+//	          for (MatchedExpression matched:matchedExpressions) {
+//	            // Print out matched text and value
+//	            System.out.println("Matched expression: " + matched.getText() + " with value " + matched.getValue());
+//	            // Print out token information
+//	            CoreMap cm = matched.getAnnotation();
+//	            for (CoreLabel token : cm.get(CoreAnnotations.TokensAnnotation.class)) {
+//	              String word = token.get(CoreAnnotations.TextAnnotation.class);
+//	              String lemma = token.get(CoreAnnotations.LemmaAnnotation.class);
+//	              String pos = token.get(CoreAnnotations.PartOfSpeechAnnotation.class);
+//	              String ne = token.get(CoreAnnotations.NamedEntityTagAnnotation.class);
+//	              System.out.println("  Matched token: " + "word="+word + ", lemma="+lemma + ", pos=" + pos + ", ne=" + ne);
+//	            }
+//	          }
+//	        }
 	        
 	        
 	    }
 		 
-	}
+	
